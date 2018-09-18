453a8481f60fd747cf0bcbd4392d87234998a915
sguillen <sguillen@cads.com.gt>
2018-09-18 16:38:51 -0600

    se importaron las rutas de de login y usuarios al app.js
    se configuro el body parser

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 //REQUIRES
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+//IMPORTAR RUTAS
+var appRoutes = require('./routes/router');
+var usuarioRoutes = require('./routes/usuariosRoutes');
+var loginRoutes = require('./routes/loginRoutes');
 
 //INICIALIZAR VARIABLES
 var app = express();
+
+//BODY PARSER
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 //CONEXION A DB
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>{
     if(err) throw err;
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
-})
-//RUTAS
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'exito'
-    });
 });
+
+//RUTAS
+app.use('/', appRoutes);
+app.use('/usuarios', usuarioRoutes);
+app.use('/login', loginRoutes);
 
 //ESCUCHAR PETICIONES
 app.listen(3500, () => {
