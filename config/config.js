36963d7d773fdf0aed1f6a759460b0f27dc44867
sguillen <sguillen@cads.com.gt>
2018-09-18 16:36:19 -0600

    Se agrego archivo js para configuracion como constatnes, se agrego la constante SEED como firma de los token
    Se agrego archivo para funcion de middleware de autenticacion y metodo para verificar token

---
+++ config/config.js
@@ -0,0 +1,1 @@
+module.exports.SEED = 'firma-del-token';
---
+++ middlewares/autenticacion.js
@@ -0,0 +1,22 @@
+var jwt = require('jsonwebtoken');
+var SEED = require('../config/config').SEED;
+
+//====================================//
+// ** Verificar Token **
+//====================================//
+exports.verificarToken = function (req, res, next){
+    var token = req.query.token;
+    jwt.verify(token, SEED, (err, decode) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                mensaje: "Token incorrecto.",
+                errors: err
+            });
+        }
+
+        req.usuarioToken = decode.usuario;
+    });
+
+    next();
+};
